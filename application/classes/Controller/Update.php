6289be90bfb732ac4c8027d917994db6ab4c4986
Hamilton Reed <hamiltonjreed@gmail.com>
2015-05-10 23:00:02 -0400

    fixing update

--- application/classes/Controller/Update.php
+++ application/classes/Controller/Update.php
@@ -5,11 +5,8 @@ class Controller_Update extends Controller {
 	public function action_index()
 	{
 		echo shell_exec("git --version")."<p>";
-		$pwd = shell_exec("pwd");
 		echo $pwd."<p>";
-		$pull = "cd ".$pwd."/fit-trackr 2>&1; git pull";
-		echo $pull."<p>";		
-		echo shell_exec("cd ".$pwd."/fit-trackr 2>&1; git pull");
+		echo shell_exec("git pull");
 		
 	}
 
