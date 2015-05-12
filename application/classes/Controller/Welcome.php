affdbc505dbe9968738d4d800b45c1d6efa715b9
Hamilton Reed <hamiltonjreed@gmail.com>
2015-05-11 22:31:01 -0400

    test page html

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -8,14 +8,52 @@ class Controller_Welcome extends Controller {
 
 	<head>
 	  <title>12M0D12 - The works of Hamilton Reed</title>
-	  <link rel=\"stylesheet\" href=\"/css/style.css\">
-	  <link rel=\"icon" type=\"image/png" href=\"http://12mod12.com/img/icon.png\">	  
+	  <link rel="stylesheet" href="/css/style.css">
+	  <link rel="icon" type="image/png" href="http://12mod12.com/img/icon.png">	  
 	</head>
-	
+
 	<body>
-		welcome to fit-trackr!  It\'s like Uber, but for fitness!!
+		<header>
+		<a href="http://12mod12.com">
+		<img src="http://12mod12.com/img/logoextended.gif" id="logo" alt="12 M0D 12 = 0">
+		</a>		
+		</header>
+		<div class="centerbody">
+			<div class="buttons">
+				<a id="leftbuffer">
+				</a>
+
+				<a id="programs" title="Programs" href="/programs/programs.html">
+					<span>Programs</span>
+				</a>
+			
+				<a id="writing" title="Writing" href="/writing/writing.html">
+					<span>Writing</span>
+				</a>
+			
+				<a id="about" title="About" href="/about/about.html">
+					<span>About</span>
+				</a>
+			</div>
+		</div>
+		<!--<div id="leftcol">
+		
+		</div>
+		
+		<div id="center">
+		
+		</div>
+		
+		<div id="rightcol">
+		
+		</div>-->
+		
+		<div id="footer">
+		
+		</div>
 	</body>
-	</html>');
+	
+</html>');
 	}
 
 } // End Welcome
