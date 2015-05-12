02c7ba8020428d6c6292abd8fc05c6d2443c1f7c
Hamilton Reed <hamiltonjreed@gmail.com>
2015-05-11 22:53:59 -0400

    Added missing semicolon

--- application/classes/Controller/Update.php
+++ application/classes/Controller/Update.php
@@ -8,7 +8,7 @@ class Controller_Update extends Controller {
 		echo shell_exec("pwd")."<p>";
 		echo shell_exec("git pull 2>&1");
 		
-		echo '\"<a href=\"/programs/fit-trackr/index.php/welcome\">Back to Home</a>'
+		echo '\"<a href=\"/programs/fit-trackr/index.php/welcome\">Back to Home</a>';
 		
 	}
 
