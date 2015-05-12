b00efb3a542e6db55017e3b3eedd5f3f0d32aba0
Hamilton Reed <hamiltonjreed@gmail.com>
2015-05-11 22:36:18 -0400

    revert welcome page - style comes later!

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -4,56 +4,7 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
-		$this->response->body('<html>
-
-	<head>
-	  <title>12M0D12 - The works of Hamilton Reed</title>
-	  <link rel="stylesheet" href="/css/style.css">
-	  <link rel="icon" type="image/png" href="http://12mod12.com/img/icon.png">	  
-	</head>
-
-	<body>
-		<header>
-		<a href="http://12mod12.com">
-		<img src="http://12mod12.com/img/logoextended.gif" id="logo" alt="12 M0D 12 = 0">
-		</a>		
-		</header>
-		<div class="centerbody">
-			<div class="buttons">
-				<a id="leftbuffer">
-				</a>
-
-				<a id="programs" title="Programs" href="/programs/programs.html">
-					<span>Lifts</span>
-				</a>
-			
-				<a id="writing" title="Writing" href="/writing/writing.html">
-					<span>Writing</span>
-				</a>
-			
-				<a id="about" title="About" href="/about/about.html">
-					<span>About</span>
-				</a>
-			</div>
-		</div>
-		<!--<div id="leftcol">
-		
-		</div>
-		
-		<div id="center">
-		
-		</div>
-		
-		<div id="rightcol">
-		
-		</div>-->
-		
-		<div id="footer">
-		
-		</div>
-	</body>
-	
-</html>');
+		$this->response->body('welcome to fit-trackr!  It\'s like Uber, but for fitness!');
 	}
 
 } // End Welcome
