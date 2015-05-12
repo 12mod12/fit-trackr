75536ab4495853986b8fc2a180573dc0135d5741
Hamilton Reed <hamiltonjreed@gmail.com>
2015-05-11 22:52:38 -0400

    Adding stable file for recursive purposes

---
+++ application/classes/Controller/Stable.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Stable extends Controller {
+
+	public function action_index()
+	{
+		echo shell_exec("git --version")."<p>";
+		echo shell_exec("pwd")."<p>";
+		echo shell_exec("git pull 2>&1");
+		
+	}
+
+}
