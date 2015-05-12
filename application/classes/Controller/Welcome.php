fe53b1e861895310f5b2166a6ca45d8b2cdc46b0
Hamilton Reed <hamiltonjreed@gmail.com>
2015-05-11 22:28:26 -0400

    Added test html

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -4,7 +4,18 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
-		$this->response->body('welcome to fit-trackr!  It\'s like Uber, but for fitness!!');
+		$this->response->body('<html>
+
+	<head>
+	  <title>12M0D12 - The works of Hamilton Reed</title>
+	  <link rel=\"stylesheet\" href=\"/css/style.css\">
+	  <link rel=\"icon" type=\"image/png" href=\"http://12mod12.com/img/icon.png\">	  
+	</head>
+	
+	<body>
+		welcome to fit-trackr!  It\'s like Uber, but for fitness!!
+	</body>
+	</html>');
 	}
 
 } // End Welcome
