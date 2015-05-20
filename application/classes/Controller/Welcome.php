497f680f57b914997a22b5e982c922ea1e40e42f
Hamilton Reed <hamiltonjreed@gmail.com>
2015-05-19 22:17:58 -0400

    adding lift type link to welcome page

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -4,7 +4,8 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
-		$this->response->body('welcome to fit-trackr!  It\'s like Uber, but for fitness!<p>  <a href=/programs/fit-trackr/index.php/lifts>Click Here to Start!</a>');
+		$this->response->body('welcome to fit-trackr!  It\'s like Uber, but for fitness!<p>  <a href=/programs/fit-trackr/index.php/lifts>Click Here to Start!</a><p> <a href=/programs/fit-trackr/index.php/liftType>Insert new lifts here.</a><p>');
+		
 	}
 
 } // End Welcome
