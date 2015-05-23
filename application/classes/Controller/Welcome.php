43da7323f30007aa084f5851b2004c24e21616d1
Hamilton Reed <hamiltonjreed@gmail.com>
2015-05-23 13:25:59 -0400

    Added header and styling to welcome page

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -1,10 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Welcome extends Controller {
+class Controller_Welcome extends Controller_Template {
+
+	public $template = 'welcome';
 
 	public function action_index()
 	{
-		$this->response->body('welcome to fit-trackr!  It\'s like Uber, but for fitness!<p>  <a href=/programs/fit-trackr/index.php/lifts>Click Here to Start!</a><p> <a href=/programs/fit-trackr/index.php/liftType>Insert new lifts here.</a><p>');
+		$this->template->message = 'Welcome to fit-trackr!';
 		
 	}
 
---
+++ application/views/welcome.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <head>
+	  <title>12M0D12 - The works of Hamilton Reed</title>
+	  <link rel="stylesheet" href="/css/style.css">
+	  <link rel="stylesheet" href="/css/programs.css">
+	  <link rel="icon" type="image/png" href="/img/icon.png">	  
+    </head>
+    <body>
+		<?php include '_includes/header.php' ?>
+    	<div class="content">
+    		<div class="description">
+        		<p><?php echo $message; ?></p>
+        		<p>It's like Uber, but for fitness!</p>
+        		<a href=/programs/fit-trackr/index.php/lifts>Click Here to Start!</a><p> 
+        		<a href=/programs/fit-trackr/index.php/liftType>Insert new lifts here.</a><p>
+        	</div>
+        </div>
+    </body>
+</html>
