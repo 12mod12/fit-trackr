7139c5a442a50a6d8dce527f900c3df62f10cf1f
Hamilton Reed <hamiltonjreed@gmail.com>
2015-12-28 19:51:07 -0700

    Clean up cardio page

--- application/views/cardio.php
+++ application/views/cardio.php
@@ -4,13 +4,16 @@
     <body>
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/scripts/header.php' ?>
 		<div class="content">
-    		<div class="submission">
+    		<div class="cardio_submission">
+    			<?php echo $message ?><p>	
+				<?php echo $result ?><p><p>
+				<a href="<?php echo $ROOT_URL ?>index.php/welcome"><== HOME</a>
         		<form class = "cardio_submission" method = "POST" action = "<?php echo $ROOT_URL ?>index.php/cardio/submit">
 					Excercise:<br> 
-					Hours:<input type="number" name="duration_hours"><br>
-					Minutes:<input type="number" name="duration_minutes"><br>
-					Seconds: <input type="number" name="duration_seconds"><br>
-					Distance: <input type="text" name="distance"><br>
+					Hours: <input type="number" name="duration_hours" style="margin-left: 22.75%;"><br>
+					Minutes: <input type="number" name="duration_minutes" style="margin-left: 18%;"><br>
+					Seconds: <input type="number" name="duration_seconds" style="margin-left: 17.75%;"><br>
+					Distance: <input type="text" name="distance" style="margin-left: 16.75%;"><br>
 					Calories Burned: <input type="number" name="burned_calories"><br>
 					<select name="cardio_type_id">
         			<?php foreach($cardio_names as $excercise): ?>
@@ -19,13 +22,10 @@
         			</select>
 					<input type="submit" value="Submit">
 				</form>
-				<?php echo $message ?><p>	
-				<?php echo $result ?><p><p>
-				<a href="<?php echo $ROOT_URL ?>index.php/welcome"><== HOME</a>
         	</div>
-        	<form class = "delete_lifts" method = "POST" action = "<?php echo $ROOT_URL ?>index.php/cardio/delete">
-        	<div class="lift_table">
-				<table name="tbl_lifts" cellpadding="1" class="lift_table_content">
+        	<form class = "delete_cardio" method = "POST" action = "<?php echo $ROOT_URL ?>index.php/cardio/delete">
+        	<div class="cardio_table">
+				<table name="tbl_lifts" cellpadding="1" class="cardio_table_content">
 					<tbody>
 						<?php foreach($cardio as $instance): ?>
 							<tr>
@@ -35,13 +35,15 @@
 								id="<?php echo $instance->cardio_id ?>">
 								<label for="<?php echo $instance->cardio_id ?>">
 								<?php $cardio = Arr::get($cardio_names,$instance->cardio_type_id) ? $cardio_names[$instance->cardio_type_id]['past_tense'] : "unknown" ?>
-								<?php echo $cardio . " " .$instance->distance ." miles in ".$instance->duration_hours.":".$instance->duration_minutes.":".$instance->duration_seconds ." burning ".$instance->burned_calories." calories on<br>".$instance->date ?></label>
+								<?php echo $cardio . " " .$instance->distance ." miles in ".$instance->duration_hours.":".$instance->duration_minutes.":".$instance->duration_seconds ." burning ".$instance->burned_calories." calories on ".$instance->date ?></label>
 							</tr>
 						<?php endforeach; ?>
 					</tbody>
 				</table>
         	</div>
-        	<input type="submit" value="Delete">
+        	<div align="center">
+        	<input type="submit" value="Delete"
+        	</div>
 			</form>
         </div>
     </body>
